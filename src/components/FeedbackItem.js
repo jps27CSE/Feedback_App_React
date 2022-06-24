b3019b37ac5f8f0e84cd4a9cf9214e74ae5ed711
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-06-24 20:24:26 +0600

    first commit

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const FeedbackItem = ({ item }) => {
+  const [rating, setRating] = useState(7);
+  const [text, setText] = useState("This is an example");
+
+  return (
+    <div className="card">
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </div>
+  );
+};
+
+export default FeedbackItem;
