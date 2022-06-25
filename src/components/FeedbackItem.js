1207c86adda1de78501e5eb1276a4bee0b7fbb45
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-06-25 13:32:06 +0600

    average

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
+import Card from "./shared/Card";
+import { FaTimes } from "react-icons/fa";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example");
 
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes
+          onClick={() => {
+            handleDelete(item.id);
+          }}
+          color="purple"
+        />
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 };
 
---
+++ src/components/shared/Card.js
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+
+const Card = ({ children, reverse }) => {
+  return <div className={`card ${reverse && "reverse"}`}>{children}</div>;
+};
+
+Card.defaultProps = {
+  reverse: false,
+};
+export default Card;
